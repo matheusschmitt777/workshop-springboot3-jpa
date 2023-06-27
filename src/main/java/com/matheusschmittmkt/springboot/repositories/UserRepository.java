0af2491590ff38523ae14f11957be9ac5cb97cde
Matheus Schmitt <matheusschmittmr45@gmail.com>
2023-06-27 20:15:53 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/matheusschmittmkt/springboot/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.matheusschmittmkt.springboot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.matheusschmittmkt.springboot.entities.User;
+
+public interface UserRepository  extends JpaRepository<User, Long>{
+
+}
