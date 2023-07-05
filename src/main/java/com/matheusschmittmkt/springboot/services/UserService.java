e4757956a8c4801751b3cdbdb3647c2f0575e2b9
Matheus Schmitt <matheusschmittmr45@gmail.com>
2023-07-04 21:11:52 -0300

    Exception handling - update

--- src/main/java/com/matheusschmittmkt/springboot/services/UserService.java
+++ src/main/java/com/matheusschmittmkt/springboot/services/UserService.java
@@ -12,6 +12,8 @@ import com.matheusschmittmkt.springboot.repositories.UserRepository;
 import com.matheusschmittmkt.springboot.services.exceptions.DatabaseException;
 import com.matheusschmittmkt.springboot.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -44,9 +46,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	
 	public void updateData(User entity, User obj) {
